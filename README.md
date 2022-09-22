4e3e811f0160b438965b25f4b0a5285344a6669b
Raiden Han <dhan5@ncsu.edu>
2022-09-22 14:06:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# two-stage-lgd
+North Carolina State University FIM 500 Fall 2022 Project
---
+++ git_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This file is used for Git training.
+
+@author: None
+"""
+
+
+def main():
+    print("Raiden's Windows has built the connection!")
+    return
+
+
+if __name__ == '__main__':
+    main()
