b79a36426f19addda3d7733e608e4ccfb5b2b330
Raiden Han <dhan5@ncsu.edu>
2022-10-05 14:28:41 -0400

    Added the visualization module

---
+++ utils/visualization.py
@@ -0,0 +1,25 @@
+def plot_distribution(s, **kwargs):
+    """ Plot the distribution of a numeric random variable
+
+    Parameters
+    ----------
+    s : Series
+        Numeric variable's sample
+
+    """
+
+    pass
+
+
+def plot_right_skewed_trans(s, **kwargs):
+    """ Plot the transformed distributions for a left-skewed variable by taking
+    cube root, square root, and logarithm
+
+    Parameters
+    ----------
+    s : Series
+        Left-skewed numeric variable's sample
+
+    """
+
+    pass
