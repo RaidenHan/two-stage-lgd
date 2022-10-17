1db8c75d5584043cf79e7967806a11980974a75a
Raiden Han <dhan5@ncsu.edu>
2022-10-17 12:46:14 -0400

    Merged Zhong's work into the visualization module

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def plot_distribution(s, **kwargs):
     """ Plot the distribution of a numeric random variable
+
+    @author: Zhong
 
     Parameters
     ----------
@@ -8,12 +17,14 @@ def plot_distribution(s, **kwargs):
 
     """
 
-    pass
+    plt.hist(s, 20, density=1)
 
 
 def plot_right_skewed_trans(s, **kwargs):
     """ Plot the transformed distributions for a left-skewed variable by taking
     cube root, square root, and logarithm
+
+    @author: Zhong
 
     Parameters
     ----------
@@ -22,4 +33,14 @@ def plot_right_skewed_trans(s, **kwargs):
 
     """
 
-    pass
+    plt.figure()
+    plt.hist(s ** (1 / 3), 20, density=1)
+    plt.show()
+
+    plt.figure()
+    plt.hist(s ** (1 / 2), 20, density=1)
+    plt.show()
+
+    plt.figure()
+    plt.hist(np.log(s), 20, density=1)
+    plt.show()
